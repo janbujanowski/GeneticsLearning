ecb2ab1eb4fe1f533b54123a25da0b987289ced0
jan.bujanowski <jan.bujanowski@gmail.com>
2019-11-27 22:13:12 +0100

    implemented RSI

--- MarketFunctions.cs
+++ MarketFunctions.cs
@@ -14,16 +14,36 @@ namespace AlgorytmEwolucyjny
             _logger = logger;
         }
 
-        public double MACDIndicator(List<DailyMarketData> data,int periods)
+        public double MACDIndicator(List<DailyMarketData> data, int periods)
         {
             if (periods != data.Count)
             {
-                _logger.LogException(new ArgumentOutOfRangeException("data", $"data.Count is {data.Count} while demanded periods is {periods}"),string.Empty);
+                _logger.LogException(new ArgumentOutOfRangeException("data", $"data.Count is {data.Count} while demanded periods is {periods}"), string.Empty);
                 return 0.0;
             }
             double macdValue = 1;
 
             return macdValue;
+        }
+        public double RSI(List<DailyMarketData> data, int periods)
+        {
+            if (periods != data.Count)
+            {
+                _logger.LogException(new ArgumentOutOfRangeException("data", $"data.Count is {data.Count} while demanded periods is {periods}"), string.Empty);
+                return 0.0;
+            }
+            double gain, loss, difference;
+            double avgGain = 0; double avgLoss = 0;
+            for (int i = 1; i < periods; i++)
+            {
+                difference = data[i].Closing - data[i - 1].Closing;
+                gain = Math.Max(0, difference);
+                loss = Math.Max(0, -difference);
+                avgGain += gain;
+                avgLoss += loss;
+            }
+
+            return 100 - 100 / (1 - (avgGain / avgLoss));
         }
     }
 }
