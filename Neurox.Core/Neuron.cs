ae3ee0d9f300b781ac378f5446ccfde1dd606370
janbujanowski <jan.bujanowski@gmail.com>
2020-04-05 17:39:10 +0200

    fix output

--- Neurox.Core/Neuron.cs
+++ Neurox.Core/Neuron.cs
@@ -6,25 +6,25 @@ namespace Neurox.Core
 {
     public class Neuron
     {
-		private List<double> _inputs;
-		private List<double> _weights;
-		private IOutputFunction _outputFunction;
+        private List<double> _inputs;
+        private List<double> _weights;
+        private IOutputFunction _outputFunction;
 
-		public List<double> Inputs
-		{
-			get { return _inputs; }
-		}
-		public List<double> Weights
-		{
-			get { return _weights; }
-		}
+        public List<double> Inputs
+        {
+            get { return _inputs; }
+        }
+        public List<double> Weights
+        {
+            get { return _weights; }
+        }
 
-		public double Output
-		{
-			get
-			{
-				return _outputFunction;
-			}
-		}
-	}
+        public double Output
+        {
+            get
+            {
+                return _outputFunction.Output(_inputs, _weights);
+            }
+        }
+    }
 }
