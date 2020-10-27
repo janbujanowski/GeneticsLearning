30c673818512e9af32c24451774a52ff6cb875da
jbujanowski <jan.bujanowski@gmail.com>
2020-10-27 17:25:35 +0100

    add dev rt compile

--- Neurox.Web/Startup.cs
+++ Neurox.Web/Startup.cs
@@ -23,8 +23,7 @@ namespace Neurox.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-                //.AddRazorRuntimeCompilation();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +32,7 @@ namespace Neurox.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+               
             }
             else
             {
